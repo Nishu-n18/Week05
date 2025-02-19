b3aa370054c9c9feeb4e5fcdd7d5a88327362c20
Nishu-n18 <yrnishu03@gmail.com>
2025-02-19 15:22:55 +0530

    [Nishu kumari] added JsonData

---
+++ JSONData/src/main/java/com/capgeminitraining/week5/StudentDetail.java
@@ -0,0 +1,19 @@
+package com.capgeminitraining.week5;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class StudentDetail {
+    public static void main(String[] args) {
+        JSONArray Subject=new JSONArray();
+        Subject.put("Maths");
+        Subject.put("English");
+        Subject.put("Computer science");
+        JSONObject student=new JSONObject();
+        student.put("name", "Nishu");
+        student.put("Age",20);
+        student.put("Subject",Subject);
+        System.out.println(student.toString());
+    }
+
+
+}
